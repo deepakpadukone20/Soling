257fd697b984efd5a43e342a068d02571680cd64
Deepak Padukone M <deepakpadukone20@live.com>
2015-10-20 00:12:21 +0530

    for jai

--- Soling.sql
+++ Soling.sql
@@ -1,10 +1,33 @@
-DROP database if exists `soling`;
+-- phpMyAdmin SQL Dump
+-- version 4.4.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 19, 2015 at 08:12 PM
+-- Server version: 5.6.26
+-- PHP Version: 5.6.12
 
-create database `soling`;
-use soling;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `soling`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
 
 CREATE TABLE IF NOT EXISTS `customer` (
-  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `address` varchar(255) DEFAULT NULL,
   `contactPersonName` varchar(100) NOT NULL,
@@ -13,64 +36,103 @@ CREATE TABLE IF NOT EXISTS `customer` (
   `isActive` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `driver`
+--
+
 CREATE TABLE IF NOT EXISTS `driver` (
-  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `address` varchar(255) NOT NULL,
   `phone` decimal(10,0) NOT NULL,
   `isOwnEmployee` tinyint(1) NOT NULL,
   `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `isActive` tinyint(1) NOT NULL,
-  `salary` decimal(10,2) NOT NULL  
+  `salary` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `expense`
+--
+
 CREATE TABLE IF NOT EXISTS `expense` (
-  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `id` int(11) NOT NULL,
   `typeId` int(11) NOT NULL,
   `date` datetime NOT NULL,
   `amount` decimal(10,2) NOT NULL,
-  `comment` varchar(500) NOT NULL,
-  KEY `typeId` (`typeId`)  
+  `comment` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS `expenseType` (
-  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `expensetype`
+--
+
+CREATE TABLE IF NOT EXISTS `expensetype` (
+  `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `isActive` tinyint(1) NOT NULL DEFAULT '1',
   `date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inventory`
+--
+
 CREATE TABLE IF NOT EXISTS `inventory` (
-  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `price` decimal(15,2) NOT NULL,
-  `qunatity` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
   `isSellable` tinyint(1) DEFAULT '1',
   `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updateDate` datetime NOT NULL,
   `isActive` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `inventory`
+--
+
+INSERT INTO `inventory` (`id`, `name`, `price`, `quantity`, `isSellable`, `date`, `updateDate`, `isActive`) VALUES
+(1, 'dasd', '3.00', 0, 1, '2015-10-18 14:40:59', '2015-10-18 14:40:59', 0),
+(2, 'asd', '34.00', 4, 1, '2015-10-18 14:41:31', '2015-10-18 14:41:31', 0),
+(3, 'jkhjkh', '8787.00', 56, 0, '2015-10-18 14:43:36', '2015-10-18 14:43:36', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int(11) NOT NULL,
+  `custId` int(11) NOT NULL,
+  `itemId` int(11) NOT NULL,
+  `driverId` int(11) NOT NULL,
+  `vehicleId` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `amount` decimal(10,2) NOT NULL,
+  `paid` decimal(10,2) NOT NULL,
+  `discount` decimal(2,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `orders` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `custId` int(11) NOT NULL,
- `itemId` int(11) NOT NULL,
- `driverId` int(11) NOT NULL,
- `vehicleId` int(11) NOT NULL,
- `quantity` int(11) NOT NULL,
- `date` datetime NOT NULL,
- `amount` decimal(10,2) NOT NULL,
- `paid` decimal(10,2) NOT NULL,
- `discount` decimal(2,2) NOT NULL,
- PRIMARY KEY (`id`),
- KEY `custId` (`custId`),
- KEY `itemId` (`itemId`),
- KEY `vehicleId` (`vehicleId`),
- KEY `driverId` (`driverId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchases`
+--
 
 CREATE TABLE IF NOT EXISTS `purchases` (
-  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `id` int(11) NOT NULL,
   `orderId` int(11) DEFAULT NULL COMMENT 'only filled when purchase is done to fullfill a specific order',
   `suppId` int(11) NOT NULL,
   `itemId` int(11) NOT NULL,
@@ -78,14 +140,17 @@ CREATE TABLE IF NOT EXISTS `purchases` (
   `date` datetime NOT NULL,
   `amount` decimal(10,2) NOT NULL,
   `paid` decimal(10,2) NOT NULL,
-  `discount` decimal(2,2) NOT NULL,
-  KEY `orderId` (orderId), 
-  KEY `suppId` (suppId), 
-  KEY `itemId` (itemId) 
+  `discount` decimal(2,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier`
+--
+
 CREATE TABLE IF NOT EXISTS `supplier` (
-  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `adddress` varchar(255) DEFAULT NULL,
   `contactPersonName` varchar(100) NOT NULL,
@@ -93,32 +158,197 @@ CREATE TABLE IF NOT EXISTS `supplier` (
   `isActive` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `isActive` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `isActive` tinyint(1) NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `password`, `email`, `isActive`, `date`) VALUES
+(1, 'admin', 'abc', 'jaitango2006@gmail.com', 1, '2015-10-18 13:21:46'),
+(3, 'jai', '$2a$10$cbb078e694c2f717ce908uZqP/RFoCAv0iHL..ApYac086J9P/.16', 'jai@gmail.com', 0, '2015-10-18 13:26:00'),
+(4, '', '$2a$10$ced93e32d5fb91fe3e6f0u9oHtLGTGBUGgogxk8M1t.3.yWLGthdC', 'admin', 0, '2015-10-18 13:48:47');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vehicle`
+--
 
 CREATE TABLE IF NOT EXISTS `vehicle` (
-  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `id` int(11) NOT NULL,
   `make` varchar(50) NOT NULL,
   `model` varchar(50) NOT NULL,
   `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `isOwnVehicle` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `driver`
+--
+ALTER TABLE `driver`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `expense`
+--
+ALTER TABLE `expense`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `typeId` (`typeId`);
+
+--
+-- Indexes for table `expensetype`
+--
+ALTER TABLE `expensetype`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `inventory`
+--
+ALTER TABLE `inventory`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `custId` (`custId`),
+  ADD KEY `itemId` (`itemId`),
+  ADD KEY `vehicleId` (`vehicleId`),
+  ADD KEY `driverId` (`driverId`);
+
+--
+-- Indexes for table `purchases`
+--
+ALTER TABLE `purchases`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `orderId` (`orderId`),
+  ADD KEY `suppId` (`suppId`),
+  ADD KEY `itemId` (`itemId`);
+
+--
+-- Indexes for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `vehicle`
+--
+ALTER TABLE `vehicle`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `driver`
+--
+ALTER TABLE `driver`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `expense`
+--
+ALTER TABLE `expense`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `expensetype`
+--
+ALTER TABLE `expensetype`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `inventory`
+--
+ALTER TABLE `inventory`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `purchases`
+--
+ALTER TABLE `purchases`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `supplier`
+--
+ALTER TABLE `supplier`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `vehicle`
+--
+ALTER TABLE `vehicle`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `expense`
+--
 ALTER TABLE `expense`
   ADD CONSTRAINT `fk_typeId` FOREIGN KEY (`typeId`) REFERENCES `expensetype` (`id`);
 
+--
+-- Constraints for table `orders`
+--
 ALTER TABLE `orders`
   ADD CONSTRAINT `fk_cust_id` FOREIGN KEY (`custId`) REFERENCES `customer` (`id`),
   ADD CONSTRAINT `fk_driver_cust_id` FOREIGN KEY (`driverId`) REFERENCES `driver` (`id`),
-  ADD CONSTRAINT `fk_vehicle_id` FOREIGN KEY (`vehicleId`) REFERENCES `vehicle` (`id`),
-  ADD CONSTRAINT `fk_item_id` FOREIGN KEY (`itemId`) REFERENCES `inventory` (`id`);
-  
+  ADD CONSTRAINT `fk_item_id` FOREIGN KEY (`itemId`) REFERENCES `inventory` (`id`),
+  ADD CONSTRAINT `fk_vehicle_id` FOREIGN KEY (`vehicleId`) REFERENCES `vehicle` (`id`);
+
+--
+-- Constraints for table `purchases`
+--
 ALTER TABLE `purchases`
   ADD CONSTRAINT `fk_purchase_item_id` FOREIGN KEY (`itemId`) REFERENCES `inventory` (`id`),
   ADD CONSTRAINT `fk_purchase_order_id` FOREIGN KEY (`orderId`) REFERENCES `orders` (`id`),
-  ADD CONSTRAINT `fk_purchase_supp_id` FOREIGN KEY (`suppId`) REFERENCES `supplier` (`id`); 
+  ADD CONSTRAINT `fk_purchase_supp_id` FOREIGN KEY (`suppId`) REFERENCES `supplier` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
